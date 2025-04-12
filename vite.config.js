8cc6fffc6da99d998ab60b2945eadc253874e1c1
Mri-dul <mriduloffi@gmail.com>
2025-04-12 23:35:17 +0530

    Build the React app

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
   server: {
     host: '0.0.0.0', // Expose the app to the local network
     port: 5173, // Default port
